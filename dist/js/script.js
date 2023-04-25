17b4eeb9a0c9d389e35c316df522406bf201b248
Kevin Kaluzny <kevin.evangelo.k@gmail>
2023-04-25 17:50:56 +0200

    Make constant thisApp in function initMenu()

--- dist/js/script.js
+++ dist/js/script.js
@@ -62,6 +62,8 @@ const select = {
 
   const app = {
     initMenu: function() {
+      const thisApp = this;
+      console.log('thisApp.data:', thisApp.data);
       const testProduct = new Product();
       console.log('testProduct:', testProduct);
     },
