5804f9af22e21976778c402a33608002d6f96063
Kevin Kaluzny <kevin.evangelo.k@gmail>
2023-04-26 18:18:18 +0200

    Make constant activeProduct

--- dist/js/script.js
+++ dist/js/script.js
@@ -92,6 +92,7 @@ const select = {
         event.preventDefault();
   
         /* find active product (product that has active class) */
+        const activeProduct = document.querySelector(menuProductsActive);
   
         /* if there is active product and it's not thisProduct.element, remove class active from it */
   
