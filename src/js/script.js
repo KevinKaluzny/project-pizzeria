025e459b25a98f5d982e5a0ad838b91a47e687eb
Kevin Kaluzny <kevin.evangelo.k@gmail>
2023-04-27 18:36:14 +0200

    Remove class active from active product

--- src/js/script.js
+++ src/js/script.js
@@ -95,6 +95,9 @@ const select = {
         const activeProduct = document.querySelector(select.all.menuProductsActive);
   
         /* if there is active product and it's not thisProduct.element, remove class active from it */
+        if (activeProduct !== null & activeProduct != thisProduct.element) {
+          activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
+        }
   
         /* toggle active class on thisProduct.element */
       });
