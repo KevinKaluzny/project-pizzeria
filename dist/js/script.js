67645e36036338742551ff54f9c99a50f7e7b111
Kevin Kaluzny <kevin.evangelo.k@gmail>
2023-05-11 19:39:16 +0200

    Create new class Cart

--- dist/js/script.js
+++ dist/js/script.js
@@ -258,6 +258,26 @@
     }
   }
 
+  class Cart {
+    constructor(element) {
+      const thisCart = this;
+
+      thisCart.products = [];
+
+      thisCart.getElements(element);
+
+      console.log('new Cart', thisCart);
+    }
+
+    getElements(element) {
+      const thisCart = this;
+
+      thisCart.dom = {};
+
+      thisCart.dom.wrapper = element;
+    }
+  }
+
   const app = {
     initMenu: function() {
       const thisApp = this;
