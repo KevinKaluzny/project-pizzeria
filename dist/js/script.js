cd3824fa5ba2f09fbea382ebdf7ab57203cdee9e
Kevin Kaluzny <kevin.evangelo.k@gmail>
2023-05-17 17:45:09 +0200

    Add property db to object settings

--- dist/js/script.js
+++ dist/js/script.js
@@ -77,6 +77,11 @@
       defaultDeliveryFee: 20,
     },
     // CODE ADDED END
+    db: {
+      url: '//localhost:3131',
+      products: 'products',
+      orders: 'orders',
+    },
   };
   
   const templates = {
