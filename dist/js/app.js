8e894fa4963595d33f40c25c6446c72327f91203
Kevin Kaluzny <kevin.evangelo.k@gmail>
2023-05-25 18:55:52 +0200

    Fix problem with missing product in cart

--- dist/js/app.js
+++ dist/js/app.js
@@ -95,7 +95,7 @@ const app = {
     thisApp.productList = document.querySelector(select.containerOf.menu);
 
     thisApp.productList.addEventListener('add-to-cart', function(event) {
-      app.cart.add(event.detail.product);
+      app.cart.add(event.detail.product.prepareCartProduct());
     });
   },
   init: function () {
