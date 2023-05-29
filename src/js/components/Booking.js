4b763c82cdca3841c634e08a2e9029b4938daa0c
Kevin Kaluzny <kevin.evangelo.k@gmail>
2023-05-29 11:44:33 +0200

    Init date picker and hour picker

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,5 +1,7 @@
 import { select, templates } from "../settings.js";
 import AmountWidget from "./AmountWidget.js";
+import DatePicker from "./DatePicker.js";
+import HourPicker from "./HourPicker.js";
 
 class Booking {
     constructor(element) {
@@ -18,11 +20,15 @@ class Booking {
 
         this.dom.peopleAmount = this.dom.wrapper.querySelector(select.booking.peopleAmount);
         this.dom.hoursAmount = this.dom.wrapper.querySelector(select.booking.hoursAmount);
+        this.dom.datePicker = this.dom.wrapper.querySelector(select.widgets.datePicker.wrapper);
+        this.dom.hourPicker = this.dom.wrapper.querySelector(select.widgets.hourPicker.wrapper);
     }
 
     initWidgets() {
         new AmountWidget(this.dom.peopleAmount);
         new AmountWidget(this.dom.hoursAmount);
+        new DatePicker(this.dom.datePicker);
+        new HourPicker(this.dom.hourPicker);
     }
 }
 
