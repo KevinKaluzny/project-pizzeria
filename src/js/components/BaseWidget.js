f3fd8d35052bc47e27486524f381c1c46a9a3589
Kevin Kaluzny <kevin.evangelo.k@gmail>
2023-05-29 10:02:17 +0200

    Create class BaseWidget

--- src/js/components/BaseWidget.js
+++ src/js/components/BaseWidget.js
@@ -5,7 +5,55 @@ class BaseWidget {
         thisWidget.dom = {};
         thisWidget.dom.wrapper = wrapperElement;
 
-        thisWidget.value = initialValue;
+        thisWidget.correctValue = initialValue;
+    }
+
+    get value() {
+        const thisWidget = this;
+
+        return thisWidget.correctValue;
+    }
+
+    set value(value) {
+        const thisWidget = this;
+
+        const newValue = thisWidget.parseValue(value);
+
+        if (thisWidget.correctValue !== newValue && thisWidget.isValid(newValue)) {
+            thisWidget.correctValue = newValue;
+            thisWidget.announce();
+        }
+
+        thisWidget.renderValue();
+    }
+
+    setValue(value) {
+        const thisWidget = this;
+
+        thisWidget.value = value;
+    }
+
+    parseValue(value) {
+        return parseInt(value);
+    }
+
+    isValid(value) {
+        return !isNaN(value);
+    }
+
+    renderValue() {
+        const thisWidget = this;
+
+        thisWidget.dom.wrapper.innerHTML = thisWidget.correctValue;
+    }
+
+    announce() {
+      const thisWidget = this;
+
+      const event = new CustomEvent('updated', {
+        bubbles: true
+      });
+      thisWidget.dom.wrapper.dispatchEvent(event);
     }
 }
 
